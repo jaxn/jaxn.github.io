1a91d15d24470cf37af1235980547cf0c02817de
Jackson Miller <jackson@resaleai.com>
2018-12-29 20:04:25 +0000

    updating Vuepress

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,11 @@ cd blog/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:jaxn/jaxn.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
